242c914c7066cffcafa79e391f1b9ea7d1f06e84
BlindedMonk <star31398@gmail.com>
2022-06-16 19:22:51 -0700

    trying to add maps

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -58,14 +58,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 children: [
                   Container(
                     height: 120,
-                    width: 120,
+                    width: 95,
                     margin: const EdgeInsets.only(
                       top: 100,
                       bottom: 8,
                     ),
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
+                      borderRadius: BorderRadius.circular(10),
                       boxShadow: const [
                         BoxShadow(
                           color: Colors.black26,
--- lib/stuff_to_be_imported/signUp.dart
+++ lib/stuff_to_be_imported/signUp.dart
@@ -25,7 +25,7 @@ class SignUpPage extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.only(bottom: 10),
                   child: Text (
-                    "Sign Up",
+                    "Hello!",
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
