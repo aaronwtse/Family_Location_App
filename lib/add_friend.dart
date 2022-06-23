ae6ae49c67f0666316761ee680e281fdfd89172f
BlindedMonk <star31398@gmail.com>
2022-06-23 01:21:12 -0700

    halfway done with friends framework

--- lib/add_friend.dart
+++ lib/add_friend.dart
@@ -7,13 +7,33 @@ class AddFriendPage extends StatefulWidget {
 }
 
 class _AddFriendPageState extends State<AddFriendPage> {
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: [
+            TextField(
+            controller: nameController,
+            obscureText: false,
+            decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText:'Name',
+            )
+            ),
 
+            TextField(
+                controller: emailController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'Email',
+                )
+            ),
+            //left of video 25 @ 8:20
           ],
         ),
       ),
